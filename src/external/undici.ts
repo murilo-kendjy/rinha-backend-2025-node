eca7d2afdf0717e1e6c189aea024d862514c525b
Murilo Kendjy Vieira Onita <muriloonita@gmail.com>
2025-08-14 16:41:42 -0300

    feat: Add undici setup

---
+++ src/external/undici.ts
@@ -0,0 +1,39 @@
+import { Pool } from "undici";
+import { PAYMENT_URL_DEFAULT, PAYMENT_URL_FALLBACK } from "../constants.js";
+
+export const poolDefault = new Pool(PAYMENT_URL_DEFAULT, {
+    connections: 10,
+    pipelining: 10,
+    keepAliveTimeout: 60000,
+});
+
+export const poolFallback = new Pool(PAYMENT_URL_FALLBACK, {
+    connections: 10,
+    pipelining: 10,
+    keepAliveTimeout: 60000,
+});
+
+export const makeRequest = async (type: 0 | 1, path: string, body?: string) => {
+    const opts: any = {
+        method: body ? "POST" : "GET",
+        headers: {},
+    };
+
+    if (body) {
+        opts.headers["content-type"] = "application/json";
+        opts.body = body;
+    }
+
+    const pool = type === 0 ? poolDefault : poolFallback;
+
+    try {
+        const { statusCode, body: responseBody } = await pool.request({
+            path,
+            ...opts,
+        });
+
+        return { statusCode, responseBody };
+    } catch (err) {
+        throw err;
+    }
+};
