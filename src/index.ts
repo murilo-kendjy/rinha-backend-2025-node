657fad74962065ea0f0f2da7f2a75a447439eeb2
Murilo Kendjy Vieira Onita <muriloonita@gmail.com>
2025-08-14 16:42:41 -0300

    feat: Create routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,78 @@
 // @ts-nocheck
 import { createServer } from "http";
 import { PORT } from "./constants.js";
+import { createGroup, redis } from "./external/keydb.js";
+import { getBalance } from "./services/payment-processor.js";
 
 async function main() {
+    await createGroup();
+
     const server = createServer((req, res) => {
-        res.writeHead(200);
-        res.end();
+        const [pathname, rawQuery] = req.url?.split("?");
+
+        if (pathname === "/payments" && req.method === "POST") {
+            let body = "";
+            const chunks: Buffer[] = [];
+            req.on("data", (chunk) => chunks.push(chunk));
+            req.on("end", () => {
+                try {
+                    redis.xadd(
+                        "payments_stream",
+                        "*",
+                        "payload",
+                        Buffer.concat(chunks).toString()
+                    );
+
+                    res.writeHead(201);
+                    res.end();
+                } catch {
+                    res.writeHead(500);
+                    res.end();
+                }
+            });
+        } else if (pathname === "/payments-summary" && req.method === "GET") {
+            const query = {};
+            if (rawQuery) {
+                for (const pair of rawQuery.split("&")) {
+                    const [key, value] = pair.split("=");
+                    query[key] = decodeURIComponent(value || "");
+                }
+            }
+
+            getBalance(
+                new Date(query.from || new Date("2025-01-01")).getTime(),
+                new Date(query.to || new Date("2026-01-01")).getTime()
+            )
+                .then((d) => {
+                    res.writeHead(200, { "Content-Type": "application/json" });
+                    res.end(JSON.stringify(d));
+                })
+                .catch(() => {
+                    res.writeHead(500);
+                    res.end();
+                });
+        } else if (pathname === "/purge-payments" && req.method === "POST") {
+            redis
+                .flushall()
+                .then(() => {
+                    createGroup()
+                        .then(() => {
+                            res.writeHead(200);
+                            res.end();
+                        })
+                        .catch(() => {
+                            res.writeHead(500);
+                            res.end();
+                        });
+                })
+                .catch(() => {
+                    res.writeHead(500);
+                    res.end();
+                });
+        } else {
+            res.writeHead(404);
+            res.end();
+        }
     });
 
     try {
