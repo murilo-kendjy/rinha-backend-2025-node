0802d4f3dcb7ecfc01ffdafca99bbd516ddf2dd5
Murilo Kendjy Vieira Onita <muriloonita@gmail.com>
2025-08-14 16:40:27 -0300

    feat: Add semaphore as util

---
+++ src/util.ts
@@ -0,0 +1,31 @@
+let current = 0;
+let queue: Array<(release: () => void) => void> = [];
+
+export const semaphore = function (max: number) {
+    const next = () => {
+        if (queue.length > 0 && current < max) {
+            current++;
+            const resolve = queue.shift()!;
+            resolve(() => {
+                current--;
+                next();
+            });
+        }
+    };
+
+    const acquire = async (): Promise<() => void> => {
+        if (current < max) {
+            current++;
+            return () => {
+                current--;
+                next();
+            };
+        }
+
+        return new Promise((resolve) => {
+            queue.push(resolve);
+        });
+    };
+
+    return { acquire };
+};
